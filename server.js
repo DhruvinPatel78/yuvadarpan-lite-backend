dc4ab94852d9199084d94fd627f1f5dcf96542c0
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2024-01-01 18:45:11 +0530

    mongo db

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT;
 
 console.log("port =>", PORT)
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+}).then(() => {
   console.log("Connected to MongoDB");
 });
 
