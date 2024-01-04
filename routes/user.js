1332945e6e871e3305306554ad714944a088d4fd
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2024-01-04 14:34:09 +0530

    api

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-router.get('/list', async(req, res) => {
-    const users = await User.find();
-    res.json(users)
-})
+router.get("/list", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
 
-router.post('/signUp', async(req, res) => {
-    const user = req.body;
-    const dbUser = await User.create(user);
-    res.send(dbUser);
-})
+router.post("/signUp", async (req, res) => {
+  const user = req.body;
+  user.password = await bcrypt.hash(user.password, 10);
+  const dbUser = await User.create(user);
+  res.send(dbUser);
+});
 
-router.post('/signIn', async(req, res) => {
-    const user = req.body;
-    const dbUser = await User.create(user);
-    res.send(dbUser);
-})
+router.post("/signIn", async (req, res) => {
+  const { email, password } = req.body;
+  const dbUser = await User.findOne({ email });
+  if (dbUser !== null && dbUser !== undefined) {
+    const passwordMatched = await bcrypt.compare(password, dbUser.password);
+    if (passwordMatched) {
+      if (dbUser?.allowed) {
+        const token = jwt.sign(
+          { email: dbUser.email, role: dbUser.role },
+          process.env.JWT_SECRET,
+        );
+        res.send({ data: dbUser, token });
+      } else {
+        res.status(403).send({ message: "Your account is not verified." });
+      }
+    } else {
+      res.status(401).send({ message: "Password or email incorrect" });
+    }
+  } else {
+    res.status(401).send({ message: "Password or email incorrect" });
+  }
+});
 
 module.exports = router;
--- routes/yuva.js
+++ routes/yuva.js
@@ -1,21 +1,41 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const Yuva = require('../models/yuva');
+const Yuva = require("../models/yuva");
 
-router.get('/list', async (req, res) => {
-    const dbYuva = await Yuva.find();
-    res.json(dbYuva);
-})
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const parsedJWT = jwt.verify(authHeader, process.env.JWT_SECRET);
+    req.user = {
+      email: parsedJWT.email,
+      role: parsedJWT.role,
+    };
+  }
+  next();
+};
 
-router.post('/:id', async(req, res) => {
-    const dbYuva = await Yuva.findById(req.params.id);
-    res.send(dbYuva);
-})
+router.use(verifyToken);
 
-router.post('/addYuva', async(req, res) => {
-    const yuva = req.body;
+router.get("/list", async (req, res) => {
+  const dbYuva = await Yuva.find();
+  res.json(dbYuva);
+});
+
+router.post("/:id", async (req, res) => {
+  const dbYuva = await Yuva.findById(req.params.id);
+  res.send(dbYuva);
+});
+
+router.post("/addYuva", async (req, res) => {
+  const yuva = req.body;
+  const user = req.user;
+  if (user.role === "ADMIN") {
     const dbYuva = await Yuva.create(yuva);
     res.send(dbYuva);
-})
+  } else {
+    res.status(403).send({ message: "Only admin can create yuva" });
+  }
+});
 
 module.exports = router;
