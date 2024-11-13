b12114c1f06bcea8deef6af67792a76c1812f632
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2024-11-13 11:27:43 +0530

    add UPDATE API for district city samaj

--- routes/district.js
+++ routes/district.js
@@ -119,4 +119,17 @@ router.get("/getInfo/:id", async (req, res) => {
   res.status(200).json(DistrictData);
 });
 
+// Update district by district id
+router.patch("/update/:id", async (req, res) => {
+  if (!errorCheck(req, res)) {
+    const { id } = req.params;
+    const payload = { ...req.body };
+    await District.updateOne(
+        { id: id },
+        { ...payload, updatedAt: new Date(), updatedBy: req?.user.id }
+    );
+    res.status(200).json({ message: "Updated Successfully" });
+  }
+});
+
 module.exports = router;
