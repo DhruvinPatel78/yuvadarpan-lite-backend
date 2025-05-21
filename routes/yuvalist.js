24d2ddcd9c7cea07c575625fcb78d9438f1786d2
mac <mac@iMac.local>
2025-05-21 12:34:24 +0530

    refactor code and fix small API issue

--- routes/yuvalist.js
+++ routes/yuvalist.js
@@ -60,7 +60,7 @@ router.get("/list", async (req, res) => {
     const page = parseInt(req.query.page, 10) || 1;
     const limit = parseInt(req.query.limit, 10) || 10;
     const offset = (page - 1) * limit;
-    const FirstName =
+    const LastName =
       lastName?.length > 0
         ? {
             lastName: { $in: lastName },
@@ -87,23 +87,22 @@ router.get("/list", async (req, res) => {
           firm: { $eq: firmName },
         }
       : {};
+    const filterSearch = {
+      ...LastName,
+      ...NativeName,
+      ...FamilyId,
+      ...Gender,
+      ...Firm,
+    };
     if (role === "ADMIN") {
       const yuvas = await Yuvalist.find({
-        ...NativeName,
-        ...FirstName,
-        ...FamilyId,
-        ...Gender,
-        ...Firm,
+        ...filterSearch,
       })
         .skip(offset)
         .limit(limit)
         .exec();
       const totalItems = await Yuvalist.countDocuments({
-        ...NativeName,
-        ...FirstName,
-        ...FamilyId,
-        ...Gender,
-        ...Firm,
+        ...filterSearch,
       });
       const totalPages = Math.ceil(totalItems / limit);
       res
@@ -114,22 +113,14 @@ router.get("/list", async (req, res) => {
       if (mangerRegion?.region) {
         const MangerYuvas = await Yuvalist.find({
           region: { $eq: mangerRegion?.region },
-          ...NativeName,
-          ...FirstName,
-          ...FamilyId,
-          ...Gender,
-          ...Firm,
+          ...filterSearch,
         })
           .skip(offset)
           .limit(limit)
           .exec();
         const managerTotalItem = await Yuvalist.find({
           region: { $eq: mangerRegion?.region },
-          ...NativeName,
-          ...FirstName,
-          ...FamilyId,
-          ...Gender,
-          ...Firm,
+          ...filterSearch,
         }).countDocuments({});
         const totalPages = Math.ceil(managerTotalItem / limit);
         res.status(200).json({
@@ -144,22 +135,14 @@ router.get("/list", async (req, res) => {
       if (mangerSamaj?.localSamaj) {
         const MangerYuvas = await Yuvalist.find({
           localSamaj: { $eq: mangerSamaj?.localSamaj },
-          ...NativeName,
-          ...FirstName,
-          ...FamilyId,
-          ...Gender,
-          ...Firm,
+          ...filterSearch,
         })
           .skip(offset)
           .limit(limit)
           .exec();
         const managerTotalItem = await Yuvalist.find({
           localSamaj: { $eq: mangerSamaj?.localSamaj },
-          ...NativeName,
-          ...FirstName,
-          ...FamilyId,
-          ...Gender,
-          ...Firm,
+          ...filterSearch,
         }).countDocuments({});
         const totalPages = Math.ceil(managerTotalItem / limit);
         res.status(200).json({
@@ -217,210 +200,6 @@ router.patch("/update/:id", async (req, res) => {
   if (!errorCheck(req, res)) {
     await Yuvalist.updateOne({ id: req.body.id }, { ...req.body });
     res.status(200).json({ message: "Updated Successfully" });
-  }
-});
-
-router.get("/requests", async (req, res) => {
-  if (!errorCheck(req, res)) {
-    const { id, role } = req.user;
-    const {
-      lastName = [],
-      state = [],
-      region = [],
-      samaj = [],
-      familyId,
-      firstName,
-      mobile,
-      email,
-      gender,
-      roles = [],
-    } = req.query;
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 10;
-    const offset = (page - 1) * limit;
-    const Roles =
-      roles?.length > 0
-        ? {
-            role: { $in: roles },
-          }
-        : {};
-    const LastName =
-      lastName?.length > 0
-        ? {
-            lastName: { $in: lastName },
-          }
-        : {};
-    const FamilyId = familyId
-      ? {
-          familyId: { $eq: familyId },
-        }
-      : {};
-    const FirstName = firstName
-      ? {
-          firstName: { $in: firstName },
-        }
-      : {};
-    const Mobile = mobile
-      ? {
-          mobile: { $eq: mobile },
-        }
-      : {};
-    const Email = email
-      ? {
-          email: { $eq: email },
-        }
-      : {};
-    const Gender = gender
-      ? {
-          gender: { $eq: gender },
-        }
-      : {};
-
-    const RegionData = await Region.findOne({
-      state_id: { $in: state },
-    });
-    const State = RegionData
-      ? {
-          region: { $eq: RegionData?.id },
-        }
-      : {};
-    const CurrentRegion =
-      region?.length > 0
-        ? {
-            region: { $in: region },
-          }
-        : {};
-    const CurrentSamaj =
-      samaj?.length > 0
-        ? {
-            localSamaj: { $in: samaj },
-          }
-        : {};
-    if (role === "ADMIN") {
-      const users = await Yuvalist.find({
-        allowed: { $eq: false },
-        active: true,
-        ...LastName,
-        ...State,
-        ...CurrentRegion,
-        ...CurrentSamaj,
-        ...FamilyId,
-        ...FirstName,
-        ...Mobile,
-        ...Gender,
-        ...Email,
-        ...Roles,
-      })
-        .skip(offset)
-        .limit(limit)
-        .exec();
-      const totalItems = await Yuvalist.find({
-        allowed: { $eq: false },
-        active: true,
-        ...LastName,
-        ...State,
-        ...CurrentRegion,
-        ...CurrentSamaj,
-        ...FamilyId,
-        ...FirstName,
-        ...Mobile,
-        ...Gender,
-        ...Email,
-        ...Roles,
-      }).countDocuments({});
-      const totalPages = Math.ceil(totalItems / limit);
-      res
-        .status(200)
-        .json({ total: totalItems, page, totalPages, data: users });
-    } else if (role === "REGION_MANAGER") {
-      const mangerRegion = await User.findById(id);
-      if (mangerRegion?.region) {
-        const MangerUsers = await Yuvalist.find({
-          allowed: { $eq: false },
-          active: true,
-          region: { $eq: mangerRegion?.region },
-          ...LastName,
-          ...State,
-          ...CurrentRegion,
-          ...CurrentSamaj,
-          ...FamilyId,
-          ...FirstName,
-          ...Mobile,
-          ...Gender,
-          ...Email,
-          ...Roles,
-        })
-          .skip(offset)
-          .limit(limit)
-          .exec();
-        const managerTotalItem = await Yuvalist.find({
-          allowed: { $eq: false },
-          active: true,
-          region: { $eq: mangerRegion?.region },
-          ...LastName,
-          ...State,
-          ...CurrentRegion,
-          ...CurrentSamaj,
-          ...FamilyId,
-          ...FirstName,
-          ...Mobile,
-          ...Gender,
-          ...Email,
-          ...Roles,
-        }).countDocuments({});
-        const totalPages = Math.ceil(managerTotalItem / limit);
-        res.status(200).json({
-          total: managerTotalItem,
-          page,
-          totalPages,
-          data: MangerUsers,
-        });
-      }
-    } else if (role === "SAMAJ_MANAGER") {
-      const mangerSamaj = await User.findById(id);
-      if (mangerSamaj?.localSamaj) {
-        const MangerUsers = await Yuvalist.find({
-          allowed: { $eq: false },
-          active: true,
-          localSamaj: { $eq: mangerSamaj?.localSamaj },
-          ...LastName,
-          ...State,
-          ...CurrentRegion,
-          ...CurrentSamaj,
-          ...FamilyId,
-          ...FirstName,
-          ...Mobile,
-          ...Gender,
-          ...Email,
-          ...Roles,
-        })
-          .skip(offset)
-          .limit(limit)
-          .exec();
-        const managerTotalItem = await Yuvalist.find({
-          allowed: { $eq: false },
-          active: true,
-          localSamaj: { $eq: mangerSamaj?.localSamaj },
-          ...LastName,
-          ...State,
-          ...CurrentRegion,
-          ...CurrentSamaj,
-          ...FamilyId,
-          ...FirstName,
-          ...Mobile,
-          ...Gender,
-          ...Email,
-          ...Roles,
-        }).countDocuments({});
-        const totalPages = Math.ceil(managerTotalItem / limit);
-        res.status(200).json({
-          total: managerTotalItem,
-          page,
-          totalPages,
-          data: MangerUsers,
-        });
-      }
-    }
   }
 });
 
